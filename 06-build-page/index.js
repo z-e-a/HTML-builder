74a08e149aff4f28bbc63211bc673d0a41e87b5d
z-e-a <e.a.zubkov@yandex.ru>
2024-01-16 22:18:28 +0300

    feat: solve task 06-build-page

---
+++ 06-build-page/index.js
@@ -0,0 +1,153 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(
+  path.join(__dirname, 'project-dist'),
+  { recursive: false },
+  function () {},
+);
+
+/////////////////////////////////////////
+
+const templateStream = new fs.ReadStream(path.join(__dirname, 'template.html'));
+
+var dataText = '';
+
+templateStream.on('readable', function () {
+  const data = templateStream.read();
+  if (data != null)
+    fs.writeFile(
+      path.join(__dirname, 'project-dist', 'index.html'),
+      data.toString(),
+      function (error) {
+        if (error) throw error;
+      },
+    );
+
+  if (data != null) {
+    dataText = data.toString();
+
+    fs.readdir(
+      path.join(__dirname, 'components'),
+      { withFileTypes: true },
+      (err, files) => {
+        if (err) console.log(err);
+        else {
+          doBundleHtmlRecursive(
+            files.filter(
+              (file) =>
+                file.isFile() &&
+                path.extname(file.name).toLowerCase() === '.html',
+            ),
+            dataText,
+          );
+        }
+      },
+    );
+  }
+});
+
+function doBundleHtmlRecursive(files = [], dataText) {
+  if (!files.length) {
+    return fs.writeFile(
+      path.join(__dirname, 'project-dist', 'index.html'),
+      dataText,
+      function (error) {
+        if (error) throw error;
+      },
+    );
+  } else {
+    let fileName = files.shift().name;
+    fs.readFile(
+      path.join(__dirname, 'components', fileName),
+      (err, componentData) => {
+        if (err) console.log(err);
+        else
+          dataText = dataText.replace(
+            `{{${path.parse(fileName).name}}}`,
+            componentData.toString(),
+          );
+        doBundleHtmlRecursive(files, dataText);
+      },
+    );
+  }
+}
+
+/////////////////////////////////////////
+
+const stylesBundle = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'style.css'),
+);
+
+fs.readdir(
+  path.join(__dirname, 'styles'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) console.log(err);
+    else {
+      streamMergeRecursive(
+        files.filter(
+          (file) =>
+            file.isFile() && path.extname(file.name).toLowerCase() === '.css',
+        ),
+        stylesBundle,
+      );
+    }
+  },
+);
+
+function streamMergeRecursive(files = [], fileWriteStream) {
+  if (!files.length) {
+    fileWriteStream.end();
+  } else {
+    const currentFile = path.resolve(__dirname, 'styles', files.shift().name);
+    const currentReadStream = fs.createReadStream(currentFile);
+
+    currentReadStream.pipe(fileWriteStream, { end: false });
+    currentReadStream.on('end', function () {
+      streamMergeRecursive(files, fileWriteStream);
+    });
+
+    currentReadStream.on('error', function (error) {
+      console.error(error);
+      fileWriteStream.close();
+    });
+  }
+}
+
+/////////////////////////////////////////
+
+function copyDirRecursive(sourceDir, destDir) {
+  fs.mkdir(destDir, { recursive: false }, (err) => {
+    if (err) console.log(err);
+    fs.readdir(path.join(sourceDir), { withFileTypes: true }, (err, files) => {
+      if (err) console.log(err);
+      else {
+        files.forEach((file) => {
+          if (file.isFile()) {
+            fs.copyFile(
+              path.join(sourceDir, file.name),
+              path.join(destDir, file.name),
+              (err) => {
+                if (err) {
+                  console.error(err);
+                }
+              },
+            );
+            return;
+          } else if (file.isDirectory()) {
+            copyDirRecursive(
+              path.join(sourceDir, file.name),
+              path.join(destDir, file.name),
+            );
+          }
+        });
+      }
+    });
+  });
+}
+
+copyDirRecursive(
+  path.join(__dirname, 'assets'),
+  path.join(__dirname, 'project-dist', 'assets'),
+);
